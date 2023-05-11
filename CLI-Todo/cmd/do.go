9d2a7558c7d6a4638140fb79e3443a244616df55
Vincent Chen <vincentchen0930@gmail.com>
2023-05-11 17:49:30 +0800

    Finished cli

--- CLI-Todo/cmd/do.go
+++ CLI-Todo/cmd/do.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"cli-todo/db"
 	"fmt"
 	"strconv"
 
@@ -24,7 +25,26 @@ var doCmd = &cobra.Command{
 				ids = append(ids, id)
 			}
 		}
-		fmt.Println(ids)
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			return
+		}
+
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err.Error())
+			} else {
+				fmt.Printf("Marked \"%d\" as completed.\n", id)
+			}
+		}
+
 	},
 }
 
--- CLI-Todo/cmd/list.go
+++ CLI-Todo/cmd/list.go
@@ -4,7 +4,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"cli-todo/db"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -14,7 +16,19 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to complete! Why not take a vacation? 🏖")
+			return
+		}
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
